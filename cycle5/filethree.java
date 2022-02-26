bf35a1198e7e3af4f4f4ba09da2764bf3c4346ba
Jasmin Jose <77009008+Jasmin0000Jose@users.noreply.github.com>
2022-02-26 23:20:17 +0530

    Add files via upload

---
+++ cycle5/filethree.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class filethree {
+    public static void main(String[] args) throws Exception {
+        try {
+
+            Scanner sc = new Scanner(System.in);
+            FileWriter fw = new FileWriter("file3.txt");
+            System.out.println("Enter the name :");
+            String name = sc.nextLine();
+            System.out.println("Enter the address :");
+            String address = sc.nextLine();
+            sc.close();
+            fw.write(name + "\n");
+            fw.write(address);
+            fw.close();
+            FileReader fr = new FileReader("javatext2.txt");
+            int ch;
+            while ((ch = fr.read()) != -1) {
+
+                System.out.print((char) ch);
+
+            }
+
+            fr.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
