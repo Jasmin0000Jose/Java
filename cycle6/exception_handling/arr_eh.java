1c67462452e617abc4ca066e073507716b7b5429
Jasmin Jose <77009008+Jasmin0000Jose@users.noreply.github.com>
2022-02-26 23:29:05 +0530

    Add files via upload

---
+++ cycle6/exception_handling/arr_eh.java
@@ -0,0 +1,13 @@
+public class arr_eh {
+
+    public static void main(String args[]) {
+
+        try {
+            int a[] = { 1, 2, 3 };
+            System.out.println(a[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
+    }
+
+}
